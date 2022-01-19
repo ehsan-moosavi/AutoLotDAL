76690ef74261cb632e67275f96ae8ce28199f339
Ehsan Moosavi <eh.moosavi@outlook.com>
2022-01-19 15:29:30 +0330

    First Commit

---
+++ AutoLotDAL/Models/Base/EntityBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace AutoLotDAL.Models.Base
+{
+   public class EntityBase
+    {
+            [Key]
+            public int Id { get; set; } 
+    }
+}
